a80aa1186d6a76d171c26c145048a805e3b053d5
ITKOO <itkoo2000@gmail.com>
2023-05-30 14:10:14 +0900

    feat(#5): 회원가입2 페이지 퍼블리싱 개발

--- src/pages/SignupInfo.js
+++ src/pages/SignupInfo.js
@@ -4,14 +4,14 @@ import BottomNextButton from "../components/BottomNextButton";
 import SignupForm from "../components/signup/SignupForm";
 import TopText from "../components/TopText";
 
-const SignUpInfo = () => {
+const SignupInfo = () => {
   return (
     <div className="App signup">
       <TopText text={["문자인증 1번만 하면", <br/>, "가입이 완료돼요!"]}></TopText>
       <SignupForm/>
-      <BottomNextButton text="다음"></BottomNextButton>
+      <BottomNextButton text="다음" linkTo="/signupMajor"></BottomNextButton>
     </div>
   );
 };
 
-export default SignUpInfo;
+export default SignupInfo;
---
+++ src/pages/SignupMajor.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../css/Signup.css';
+import BottomNextButton from "../components/BottomNextButton";
+import TopText from "../components/TopText";
+import TopSmallText from "../components/TopSmallText";
+import MajorButtons from "../components/signup/MajorButtons";
+
+const SignupMajor = () => {
+  return (
+    <div className="App signup">
+      <TopText text={["경력 또는 관심이 있는", <br/>, "직종을 선택해 주세요 😁"]}></TopText>
+      <TopSmallText text={["선택해 주시면 저희가", <br/>, "스마트하게 회사를 추천해 드릴께요."]}></TopSmallText>
+      <MajorButtons/>
+      <BottomNextButton text="다음" linkTo="/signupMajor"></BottomNextButton>
+    </div>
+  );
+};
+
+export default SignupMajor;
