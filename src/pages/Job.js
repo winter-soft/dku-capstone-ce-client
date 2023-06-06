eb0a54b123cb934787b992e7b181506f16a1a61b
ITKOO <itkoo2000@gmail.com>
2023-06-06 22:00:39 +0900

    feat(#12): 상세화면 퍼블리싱 개발

--- src/pages/Job.js
+++ src/pages/Job.js
@@ -1,13 +1,20 @@
 import React from "react";
 import '../css/Job.css';
 import Header from "../components/common/Header";
-import BottomMenu from "../components/common/BottomMenu";
+import JobContent from "../components/job/JobContent";
+import BottomDoubleButtons from "../components/common/BottomDoubleButtons";
+import ShareButton from "../components/common/ShareButton";
 
 const Job = () => {
+  const imgExampleUrl = 'https://webzine.kccworld.co.kr/data/cn/87/7.jpg';
+
   return (
     <div className="App">
       <Header/>
-      <BottomMenu activeMenu="job"/>
+      <img src={imgExampleUrl} alt="" className="jobImg"/>
+      <ShareButton/>
+      <JobContent/>
+      <BottomDoubleButtons/>
     </div>
   );
 };
